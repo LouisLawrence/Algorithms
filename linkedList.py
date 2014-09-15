eb4ebf12bcfa1d738ccd58f7e63433f9be3b3295
Louis Lawrence <34742@longroad.ac.uk>
2014-09-15 16:06:56 +0100

    New linked lists activity to ad to the bottom of recursion homework

---
+++ linkedList.py
@@ -0,0 +1,60 @@
+class Node:
+    """Node"""
+    def __init__(self,data):
+        self.data = data
+        self.nextPointer = None
+
+    def GetData(self):
+        return self.data
+    def SetData(self):
+        self.data = newData
+    def GetNextPointer(self):
+        return self.nextPointer
+    def SetNextPointer(self):
+        self.nextPointer = newPointer
+        
+        
+        
+class Unordered_List:
+    """Unordered List"""
+    def __init__(self):
+        self.head = None
+
+    def isEmpty(self):
+        if self == None:
+            return True
+        else:
+            return False
+    def add(self):
+        temp = Node(data)
+        Node.nextPointer
+        self.head = temp
+    def getList(self):
+
+    def length(self):
+        length = len(head)
+        return length
+    def search(self):
+        current = Node
+        found = False
+        while current != None adn found == False:
+            if Node.Getdata
+    def remove(self):
+        #get next pointer of del node
+        #change next pointer to that of del node
+        
+
+class Ordered_List:
+    """Ordered List"""
+    def search(self):
+        itemToSearch = 2
+        
+        #check first item then next
+        #loop
+        
+    def add(self):
+        New_Node.GetData()
+
+
+if __name__ == "__main__":
+    
